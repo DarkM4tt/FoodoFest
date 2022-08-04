ceb56b6079bf3f00baf374a2da616265c4adf402
Prabhat Mani Srivastava <prabhatmani933@gmail.com>
2022-08-04 10:51:19 +0530

    Added checkout component to render form when someone clicks order.

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,7 +1,8 @@
 import { useContext, useEffect, useState } from 'react'
+
 import CartIcon from '../Cart/CartIcon'
-import classes from './HeaderCartButton.module.css'
 import CartContext from '../../store/cart-context'
+import classes from './HeaderCartButton.module.css'
 
 const HeaderCartButton = (props) => {
   const [btnIsHighlighted, setBtnIsHighlighted] = useState(false)
@@ -9,14 +10,14 @@ const HeaderCartButton = (props) => {
 
   const { items } = cartCtx
 
-  const numberOfCartItems = cartCtx.items.reduce((currNumber, item) => {
-    return currNumber + item.amount
+  const numberOfCartItems = items.reduce((curNumber, item) => {
+    return curNumber + item.amount
   }, 0)
 
   const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ''}`
 
   useEffect(() => {
-    if (cartCtx.items.length === 0) {
+    if (items.length === 0) {
       return
     }
     setBtnIsHighlighted(true)
